d25b15cbd6fe4daf29038dd9efe39d2bd2077bcd
tom <tom@service.com>
2018-11-08 23:33:27 +0800

    ok

--- src/main/java/com/baidu/rpc/RpcClient.java
+++ src/main/java/com/baidu/rpc/RpcClient.java
@@ -19,16 +19,20 @@ public class RpcClient {
         Connection connection = new ConnectionBuilder().getConnection();
         Channel channel = connection.createChannel();
         String replyTo = "client_replyTo";
+        //声明回调队列
         channel.queueDeclare(replyTo, false, false, true, null);
+        //监听回调队列
         Consumer consumer = new DefaultConsumer(channel) {
 
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 long deliveryTag = envelope.getDeliveryTag();
+                //判断响应是否属于自己, 不属于则拒绝
                 if (!correlationId.equals(properties.getCorrelationId())) {
                     channel.basicReject(deliveryTag, true);
                     return;
                 }
+                //响应属于自己, 获取响应数据, 确认消息
                 System.out.println(new String(body));
                 channel.basicAck(deliveryTag, false);
                 connection.close(200, "exit success");
@@ -39,6 +43,7 @@ public class RpcClient {
 
         channel.basicConsume(replyTo, consumer);
 
+        //发送消息到requestQueue, 并设置replyTo, correlationId
         channel.basicPublish("", requestQueue
                 , new AMQP.BasicProperties().builder().replyTo(replyTo).correlationId(correlationId).build()
                 , "client request".getBytes());
--- src/main/java/com/baidu/rpc/RpcServer.java
+++ src/main/java/com/baidu/rpc/RpcServer.java
@@ -2,6 +2,7 @@ package com.baidu.rpc;
 
 import com.baidu.ConnectionBuilder;
 import com.rabbitmq.client.*;
+import com.rabbitmq.client.impl.AMQBasicProperties;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
@@ -20,15 +21,20 @@ public class RpcServer {
         String requestQueue = "requestQueue";
         Connection connection = new ConnectionBuilder().getConnection();
         final Channel channel = connection.createChannel();
+        //声明requestQueue
         channel.queueDeclare(requestQueue, false, false, true, null);
+        //监听requestQueue
         Consumer consumer = new DefaultConsumer(channel) {
 
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 try {
+                    //获取请求消息
                     System.out.println("receive request data is " + new String(body));
                 } finally {
+                    //把响应消息发送到回调队列properties.getReplyTo(),并使用请求的属性参数
                     channel.basicPublish("", properties.getReplyTo(), properties, "response data".getBytes());
+                    //确认消息
                     channel.basicAck(envelope.getDeliveryTag(), false);
                 }
             }
